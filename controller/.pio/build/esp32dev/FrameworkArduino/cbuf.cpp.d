dbafe7c3a5d43c8ff25df63b873c97841a538131
Phan Trung Nhựt <173559382+nhutphansayhi@users.noreply.github.com>
2025-12-09 21:31:19 +0700

    add

---
+++ controller/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ /Users/nhutphan/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/nhutphan/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
